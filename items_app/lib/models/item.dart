567193cd745e005f6d25a53168af469c6ed70240
udaie <udaisenevirathne@gmail.com>
2025-07-16 21:16:27 +0530

    remove unused analysis options and widget test files

--- items_app/lib/models/item.dart
+++ items_app/lib/models/item.dart
@@ -10,13 +10,4 @@ class Item {
     required this.description,
     required this.imageUrl,
   });
-
-  factory Item.fromJson(Map<String, dynamic> json) {
-    return Item(
-      id: json['id'],
-      title: json['title'],
-      description: json['description'],
-      imageUrl: json['imageUrl'],
-    );
-  }
 }
